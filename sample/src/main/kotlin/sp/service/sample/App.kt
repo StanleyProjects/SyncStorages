2c0aee595b919cf7d3652cfcbf0ec69ba9994a92
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-22 16:34:17 +0300

    fix sample

--- sample/src/main/kotlin/sp/service/sample/App.kt
+++ sample/src/main/kotlin/sp/service/sample/App.kt
@@ -1,10 +1,12 @@
 package sp.service.sample
 
 import sp.kx.bytes.Transformer
-import sp.kx.hashes.HashFunction
+import sp.kx.hashes.Hashes
+import sp.kx.ids.RealIds
 import sp.kx.storages.RealSyncStorage
 import sp.kx.storages.SyncStorage
 import sp.kx.streamers.MutableFileStreamer
+import sp.kx.times.RealTimes
 import java.io.File
 import java.util.UUID
 
@@ -22,31 +24,33 @@ fun main() {
         id = UUID.randomUUID(),
         streamer = MutableFileStreamer(File.createTempFile("foo", "bar")),
         transformer = transformer,
-        hf = HashFunction.MD5,
+        hashes = Hashes.MD5,
+        times = RealTimes(),
+        ids = RealIds(),
     )
     println("storage: ${storage.id}")
-    check(storage.items.isEmpty())
+    check(storage.payloads.isEmpty())
     //
     val p0 = storage.add("foo")
     println("item: ${p0.valueInfo}")
-    check(storage.items.size == 1)
+    check(storage.payloads.size == 1)
     check(storage[p0.valueInfo.id]!!.value == "foo")
     val p1 = storage.add("bar")
     println("item: ${p1.valueInfo}")
-    check(storage.items.size == 2)
+    check(storage.payloads.size == 2)
     check(storage[p1.valueInfo.id]!!.value == "bar")
     val p2 = storage.add("baz")
     println("item: ${p2.valueInfo}")
-    check(storage.items.size == 3)
+    check(storage.payloads.size == 3)
     check(storage[p2.valueInfo.id]!!.value == "baz")
     //
-    val v1 = storage.set(p1.valueInfo.id, "qux") // todo update
+    val v1 = storage.update(id = p1.valueInfo.id, value = "qux")
     checkNotNull(v1)
-    check(storage.items.size == 3)
+    check(storage.payloads.size == 3)
     check(storage[p1.valueInfo.id]!!.value == "qux")
     //
     check(storage.delete(p2.valueInfo.id))
-    check(storage.items.size == 2)
+    check(storage.payloads.size == 2)
     check(storage[p0.valueInfo.id]!!.value == "foo")
     check(storage[p1.valueInfo.id]!!.value == "qux")
 }
