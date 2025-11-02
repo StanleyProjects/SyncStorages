2466d012a144657c0d71974252b7a2b4cceef6cc
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-11-02 16:44:50 +0300

    fix code style

--- lib/src/main/kotlin/sp/kx/storages/SyncStorage.kt
+++ lib/src/main/kotlin/sp/kx/storages/SyncStorage.kt
@@ -14,7 +14,6 @@ import sp.kx.times.Times
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.io.OutputStream
-import java.util.Collections
 import java.util.UUID
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
