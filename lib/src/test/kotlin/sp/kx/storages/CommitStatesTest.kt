766b1ad0d741a5568ce68b5ef7930e26fb24f924
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-11-05 01:45:46 +0300

    add

--- lib/src/test/kotlin/sp/kx/storages/CommitStatesTest.kt
+++ lib/src/test/kotlin/sp/kx/storages/CommitStatesTest.kt
@@ -3,9 +3,58 @@ package sp.kx.storages
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
+import java.io.Serializable
 import java.util.HexFormat
 
 internal class CommitStatesTest {
+    @Test
+    fun addTest(@TempDir dir: File) {
+        val testSuite = SyncStoragesTestSuite(dir = dir)
+        //
+        setOf(String::class.java, Int::class.java).forEach { type ->
+            testSuite.add(index = 0, type = type, count = 2)
+            testSuite.add(index = 1, type = type, count = 2)
+        }
+        val p000 = testSuite.payload<String>(0, 0)
+        val p001 = testSuite.payload<String>(0, 1)
+        val p010 = testSuite.payload<Int>(0, 0)
+        val p011 = testSuite.payload<Int>(0, 1)
+        //
+        val p100 = testSuite.payload<String>(1, 0)
+        val p101 = testSuite.payload<String>(1, 1)
+        val p110 = testSuite.payload<Int>(1, 0)
+        val p111 = testSuite.payload<Int>(1, 1)
+        //
+        testSuite.assertEquals(0, listOf(p000, p001))
+        testSuite.assertEquals(0, listOf(p010, p011))
+        testSuite.assertEquals(1, listOf(p100, p101))
+        testSuite.assertEquals(1, listOf(p110, p111))
+        //
+        val s0SyncStates = testSuite.storages(0).getSyncStates()
+        val s1MergeStates = testSuite.storages(1).getMergeStates(s0SyncStates)
+        testSuite.merge(
+            index = 0,
+            mergeStates = s1MergeStates,
+            expected = mapOf(
+                testSuite.storage<String>(0).id to testSuite.commitState(
+                    gives = listOf(p000, p001),
+                    expected = listOf(p000, p001, p100, p101),
+                    transform = Transformers.Strings::map,
+                ),
+                testSuite.storage<Int>(0).id to testSuite.commitState(
+                    gives = listOf(p010, p011),
+                    expected = listOf(p010, p011, p110, p111),
+                    transform = Transformers.Ints::map,
+                ),
+            ),
+        )
+        //
+        testSuite.assertEquals(0, listOf(p000, p001, p100, p101))
+        testSuite.assertEquals(0, listOf(p010, p011, p110, p111))
+        testSuite.assertEquals(1, listOf(p100, p101))
+        testSuite.assertEquals(1, listOf(p110, p111))
+    }
+
     @Test
     fun s1MergeTest(@TempDir dir: File) {
         val testSuite = SyncStoragesTestSuite(dir = dir)
--- lib/src/test/kotlin/sp/kx/storages/SyncStoragesTestSuite.kt
+++ lib/src/test/kotlin/sp/kx/storages/SyncStoragesTestSuite.kt
@@ -12,6 +12,7 @@ internal class SyncStoragesTestSuite {
     val times: Times
     val ids: Ids
     private val stack: List<SyncStorages>
+    val s0: SyncStorages get() = stack[0]
     val s1: SyncStorages get() = stack[1]
     val s2: SyncStorages get() = stack[2]
 
@@ -58,16 +59,53 @@ internal class SyncStoragesTestSuite {
         return SyncStorageAlgorithms.hashOf(payloads = payloads.sortedWith(Comparators.payloads), hashes = hashes)
     }
 
-    fun add(index: Int, value: String): Payload<String> {
-        val storage = storage<String>(index = index)
-        val payload = storage.add(value = value)
-        check(payload.value == value)
-        check(payload.created == payload.updated)
-        return payload
+    private var strings = 0
+    private var ints = 0
+
+    fun merge(index: Int, mergeStates: Map<UUID, MergeState>, expected: Map<UUID, CommitState>) {
+        assertEquals(
+            expected = expected,
+            actual = storages(index = index).merge(mergeStates),
+            assert = { _, expected, actual -> expected.assertEquals(actual = actual) },
+        )
     }
 
-    fun add(index: Int, value: Int): Payload<Int> {
-        val storage = storage<Int>(index = index)
+    fun <T : Comparable<T>> commitState(
+        deleted: Set<UUID> = emptySet(),
+        gives: List<Payload<T>> = emptyList(),
+        expected: List<Payload<T>>,
+        transform: (Payload<T>) -> Payload<ByteArray>,
+    ): CommitState {
+        return CommitState(
+            deleted = deleted,
+            gives = gives.map(transform),
+            hash = hashOf(payloads = expected.map(transform)),
+        )
+    }
+
+    fun <T : Any> add(index: Int, type: Class<out T>, count: Int): List<Payload<T>> {
+        return (0 until count).map { add(index = index, type = type) }
+    }
+
+    fun <T : Any> add(index: Int, type: Class<out T>): Payload<T> {
+        return when (type) {
+            String::class.java -> {
+                add(index = index, value = "value:${strings++}")
+            }
+            Integer::class.java, Int::class.java -> {
+                val value = 1_000_000 + ints++
+                add(index = index, value = value)
+            }
+            else -> error("Type $type is not supported!")
+        } as Payload<T>
+    }
+
+    inline fun <reified T : Any> add(index: Int): Payload<T> {
+        return add(index = index, type = T::class.java)
+    }
+
+    inline fun <reified T : Comparable<T>> add(index: Int, value: T): Payload<T> {
+        val storage = storage<T>(index = index)
         val payload = storage.add(value = value)
         check(payload.value == value)
         check(payload.created == payload.updated)
@@ -107,11 +145,33 @@ internal class SyncStoragesTestSuite {
         return after
     }
 
-    fun storages(index: Int): MutableStorages {
-        return stack.getOrElse(index) { error("No storages by index: $index!") }
+    fun storages(index: Int): SyncStorages {
+        return stack.getOrElse(index = index) { error("No storages by index: $index!") }
     }
 
-    inline fun <reified T : Any> storage(index: Int): MutableStorage<T> {
+    fun <T : Comparable<T>> storage(index: Int, type: Class<T>): MutableStorage<T> {
+        return storages(index = index)[type] ?: error("No storage($index/${type.name})!")
+    }
+
+    inline fun <reified T : Comparable<T>> storage(index: Int): MutableStorage<T> {
         return storages(index = index)[T::class.java] ?: error("No storage($index/${T::class.java.name})!")
+    }
+
+    inline fun <reified T : Comparable<T>> payload(storage: Int, index: Int): Payload<T> {
+        return storage<T>(index = storage).payloads.getOrElse(index = index) {
+            error("No payload by index $index in storage $storage/${T::class.java.name}!")
+        }
+    }
+
+    internal inline fun <reified T : Comparable<T>> assertEquals(
+        index: Int,
+        expected: Collection<Payload<T>>,
+    ) {
+        assertEquals(
+            expected = expected,
+            actual = storage<T>(index).payloads,
+            comparator = Comparators.payloads,
+            assert = { _, e, a -> assertEquals(expected = e, actual = a) },
+        )
     }
 }
