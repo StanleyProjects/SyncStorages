7b1c0f9991aded5aa4ad556e850ea4b9b5e5aa9a
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-11-05 15:58:22 +0300

    wip

---
+++ lib/src/test/kotlin/sp/kx/storages/ValueStateTest.kt
@@ -0,0 +1,27 @@
+package sp.kx.storages
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.milliseconds
+
+internal class ValueStateTest {
+    @Test
+    fun toStringTest() {
+        val issuer = ValueState(
+            updated = 1.milliseconds,
+            hash = byteArrayOf(4, 3, 2, 1),
+        )
+        val expected = "ValueState(updated: 1, hash: 4)"
+        assertEquals(expected, issuer.toString())
+    }
+
+    @Test
+    fun hashCodeTest() {
+        val issuer = ValueState(
+            updated = 1.milliseconds,
+            hash = byteArrayOf(4, 3, 2, 1),
+        )
+        val expected = 63046592
+        assertEquals(expected, issuer.hashCode())
+    }
+}
