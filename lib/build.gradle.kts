8b45147dd937deec3a5fc912cbd4a92560b1901c
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-11-21 18:33:08 +0300

    Storages:0.14.1u-SNAPSHOT

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     implementation("com.github.kepocnhh:Bytes:0.4.1u-SNAPSHOT")
     implementation("com.github.kepocnhh:Hashes:0.1.0-SNAPSHOT")
     implementation("com.github.kepocnhh:Ids:0.0.1-SNAPSHOT")
-    implementation("com.github.kepocnhh:Storages:0.14.0u-SNAPSHOT")
+    implementation("com.github.kepocnhh:Storages:0.14.1u-SNAPSHOT")
     implementation("com.github.kepocnhh:Streamers:0.1.0-SNAPSHOT")
     implementation("com.github.kepocnhh:Times:0.0.1-SNAPSHOT")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${Version.jupiter}")
