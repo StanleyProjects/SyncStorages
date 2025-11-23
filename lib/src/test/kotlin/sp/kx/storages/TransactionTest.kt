5a7df6c5511a9f3595247d86ea0efc27db9c4510
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-11-23 16:51:59 +0300

    tests

--- lib/src/test/kotlin/sp/kx/storages/TransactionTest.kt
+++ lib/src/test/kotlin/sp/kx/storages/TransactionTest.kt
@@ -7,7 +7,7 @@ import kotlin.time.Duration.Companion.seconds
 
 internal class TransactionTest {
     @Test
-    fun addTest(@TempDir dir: File) {
+    fun defaultTest(@TempDir dir: File) {
         val testSuite = SyncStoragesTestSuite(dir = dir)
         val builder = RealSyncStorages.Builder()
             .add(Keys.Strings, Transformers.Strings)
@@ -21,6 +21,63 @@ internal class TransactionTest {
             storage = testSuite.storage(storages, key = Keys.Durations),
             expected = emptyList(),
         )
+        var transaction = Transaction.Builder()
+            .add(Keys.Strings, "foo")
+            .add(Keys.Strings, "bar")
+            .add(Keys.Durations, 42.seconds)
+            .add(Keys.Durations, 43.seconds)
+            .build()
+        storages.commit(transaction = transaction)
+        Keys.Strings.also { key ->
+            val payloads = testSuite.storage(storages, key = key).payloads
+            val values = payloads.map { it.value }
+            testSuite.assertEquals(
+                expected = listOf("foo", "bar"),
+                actual = values,
+            )
+        }
+        Keys.Durations.also { key ->
+            val payloads = testSuite.storage(storages, key = key).payloads
+            val values = payloads.map { it.value }
+            testSuite.assertEquals(
+                expected = listOf(42.seconds, 43.seconds),
+                actual = values,
+            )
+        }
+        val s0 = testSuite.storage(storages, key = Keys.Strings).payloads.firstOrNull { it.value == "bar" } ?: error("No payload!")
+        val d0 = testSuite.storage(storages, key = Keys.Durations).payloads.firstOrNull { it.value == 43.seconds } ?: error("No payload!")
+        transaction = Transaction.Builder()
+            .add(Keys.Strings, "baz")
+            .delete(Keys.Strings, id = s0.id)
+            .add(Keys.Durations, 44.seconds)
+            .delete(Keys.Durations, id = d0.id)
+            .build()
+        storages.commit(transaction = transaction)
+        Keys.Strings.also { key ->
+            val payloads = testSuite.storage(storages, key = key).payloads
+            val values = payloads.map { it.value }
+            testSuite.assertEquals(
+                expected = listOf("foo", "baz"),
+                actual = values,
+            )
+        }
+        Keys.Durations.also { key ->
+            val payloads = testSuite.storage(storages, key = key).payloads
+            val values = payloads.map { it.value }
+            testSuite.assertEquals(
+                expected = listOf(42.seconds, 44.seconds),
+                actual = values,
+            )
+        }
+    }
+
+    @Test
+    fun addTest(@TempDir dir: File) {
+        val testSuite = SyncStoragesTestSuite(dir = dir)
+        val builder = RealSyncStorages.Builder()
+            .add(Keys.Strings, Transformers.Strings)
+            .add(Keys.Durations, Transformers.Durations)
+        val storages = testSuite.storages(builder = builder)
         val transaction = Transaction.Builder()
             .add(Keys.Strings, "foo")
             .add(Keys.Strings, "bar")
